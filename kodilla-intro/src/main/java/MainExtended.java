bae2d43a8137ecb54d23fc1da4336d90ad3b077b
Tomasz Predkiewicz <tomasz.predkiewicz@gmail.com>
2022-12-29 14:28:30 +0100

    Main, Main Extended, Hello World, First Class updated

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args){
+        String tankName = "Leopard2PL";
+        String tankType = "MBT";
+        int numberOfUnits = 24;
+        char tankSymbol = 'H';
+        double armorDensity = 4.20;
+        boolean isItAdvanced = true;
+
+        System.out.println(tankName);
+        System.out.println(tankType);
+        System.out.println(numberOfUnits);
+        System.out.println(tankSymbol);
+        System.out.println(armorDensity);
+        System.out.println(isItAdvanced);
+
+    }
+}
