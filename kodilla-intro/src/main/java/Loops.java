f3c8295e63548195b2e5c0d947da3de8c09fb238
Tomasz Predkiewicz <tomasz.predkiewicz@gmail.com>
2023-01-02 19:57:06 +0100

    Loops added

---
+++ kodilla-intro/src/main/java/Loops.java
@@ -0,0 +1,20 @@
+public class Loops {
+    public static void main(String args[]){
+
+        int [] numbers = new int [3];
+
+        numbers [0] = 11;
+        numbers [1] = 24;
+        numbers [2] = 36;
+
+        System.out.println(sumNumbers(numbers));
+    }
+    public static int sumNumbers(int[] numbers){
+        int result = 0;
+        for(int i =0; i < numbers.length; i++)
+        result = result + numbers[i];
+        return result;
+    }
+
+    }
+
