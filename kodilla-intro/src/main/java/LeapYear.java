c2e6a150502696a4bfc57a4df5f00cd5b3081aee
Tomasz Predkiewicz <tomasz.predkiewicz@gmail.com>
2023-01-04 11:18:47 +0100

    Added Better LeapYear

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,12 +1,13 @@
-public class LeapYear {
-    public static void main(String[] args){
-        int myYear = 2024;
-        boolean leapYear = false;
+class LeapYear {
+    public static void main(String args[]) {
+        int myYear = 2020;
 
-        if((myYear%4==0) || (myYear%400==0)){
-            leapYear = true;
+        if(myYear%4==0 && myYear%100 !=0){
+            System.out.println("To jest Rok przestępny");
+        } else if (myYear%400==0) {
+            System.out.println("To też jest rok przestępny");
+        } else
+        { System.out.println("To jest rok zwykły");
         }
-        System.out.println("Year " + myYear + " is Leap Year: " + leapYear);
-
     }
 }
