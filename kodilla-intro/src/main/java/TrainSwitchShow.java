7ee4f90c6e1500502703f0e3ffd35c1a5afc7df7
Tomasz Predkiewicz <tomasz.predkiewicz@gmail.com>
2023-01-20 10:41:01 +0100

    TrainSwitch amended

--- kodilla-intro/src/main/java/TrainSwitchShow.java
+++ kodilla-intro/src/main/java/TrainSwitchShow.java
@@ -2,6 +2,5 @@ public class TrainSwitchShow {
     public static void main(String[] args) {
         TrainSwitch name = new TrainSwitch();
         System.out.println(name.getColorSelection());
-
     }
 }
