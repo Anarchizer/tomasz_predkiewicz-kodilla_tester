e4dc1b1e7a6b6354bc671aa025ef498026f115c1
Tomasz Predkiewicz <tomasz.predkiewicz@gmail.com>
2023-01-26 15:17:43 +0100

    Book corrections

--- kodilla-intro/src/main/java/Book.java
+++ kodilla-intro/src/main/java/Book.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
-
 public class Book {
     private String author;
     private String title;
 
-    public Book (String author, String title){
-        this.author = author;
-        this.title = title;
-    }
+// public Book (String author, String title){
+// this.author = author;
+// this.title = title;
+// }
+
     public static Book of(String author, String title) {
-    Book book = new Book(title, author);
-    return book;
+        Book book = new Book();
+        book.author = author;
+        book.title = title;
+        return book;
     }
 }
