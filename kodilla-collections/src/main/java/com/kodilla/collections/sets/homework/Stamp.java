283f8bac9e46f27cb732898675a010a1b9bb2f58
Tomasz Predkiewicz <tomasz.predkiewicz@gmail.com>
2023-02-05 09:30:30 +0100

    Stamp homework

---
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/Stamp.java
@@ -0,0 +1,61 @@
+package com.kodilla.collections.sets.homework;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class Stamp {
+    private String name;
+    private double height;
+    private double width;
+    private String stamped;
+    private String notStamped;
+
+    public Stamp(String name, double height, double width){
+        this.name = name;
+        this.height = height;
+        this.width = width;
+        this.stamped = "Stamped";
+        this.notStamped = "Not Stamped";
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public String isStamped() {
+        if(height > 3){
+            return stamped;
+        } else return notStamped;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Stamp stamp = (Stamp) o;
+        return Double.compare(stamp.height, height) == 0 && Double.compare(stamp.width, width) == 0 && Objects.equals(name, stamp.name) && Objects.equals(stamped, stamp.stamped) && Objects.equals(notStamped, stamp.notStamped);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, height, width, stamped, notStamped);
+    }
+
+    @Override
+    public String toString() {
+        return "Stamp{" +
+                "name='" + name + '\'' +
+                ", height=" + height +
+                ", width=" + width +
+                '}';
+    }
+}
---
+++ kodilla-collections/src/main/java/com/kodilla/collections/sets/homework/StampsApplication.java
@@ -0,0 +1,23 @@
+package com.kodilla.collections.sets.homework;
+
+import com.kodilla.collections.sets.Order;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StampsApplication {
+    public static void main(String[] args) {
+        Set<Stamp> stamps = new HashSet<>();
+
+        stamps.add(new Stamp("Hungary", 2 ,2));
+        stamps.add(new Stamp("Poland",4,2));
+        stamps.add(new Stamp("Germany",6,4));
+        stamps.add(new Stamp("Slovakia",1,4));
+        stamps.add(new Stamp("Germany",6,4));
+
+        System.out.println(stamps.size());
+        for (Stamp stamp : stamps)
+            System.out.println(stamp + stamp.isStamped());
+
+    }
+}
